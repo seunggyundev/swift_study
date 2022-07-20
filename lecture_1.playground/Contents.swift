a43dbeea7d253c46572e857d8c0b5b71df12ea62
seunggyun_jang <seunggyun2743@gmail.com>
2022-07-20 20:58:33 +0900

    2 commit

--- lecture_1.playground/Contents.swift
+++ lecture_1.playground/Contents.swift
@@ -1,3 +1,20 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//변수
+var str = "Hello, playground"
+var age = 10
+
+age = 20
+str = "hi"  //큰따음표가 string
+
+// Swift 언어 특징
+// Safe type 타입 안전한,강타입,타입이 엄격하다라고 하며, 타입세잎에 대해 어떻게 생각하는지 면접에서 물어보는 경우가 있다
+// Safe type하지 않은 언어 : javaScript
+
+
+//상수 : 값이 변하지 않음
+let guestName = "john"
+
+
+
+
